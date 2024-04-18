7fb67a01f36a70f11dab1f7f3342f124a6bdb051
Jagrit Chawla <jagritchawla2003@example.com>
2024-04-18 19:19:32 +0530

    switched to react-chat-engine-pretty

--- src/ChatsPage.jsx
+++ src/ChatsPage.jsx
@@ -1,33 +1,26 @@
 // import { MultiChatWindow,MultiChatSocket, useMultiChatLogic } from 'react-chat-engine-advanced';
-// import { PrettyChatWindow} from 'react-chat-engine-pretty'
-
-import { ChatEngine } from 'react-chat-engine'
+import { PrettyChatWindow} from 'react-chat-engine-pretty'
 
 export const ChatsPage = ({ user }) => {
     const projectId = import.meta.env.VITE_PROJECT_ID;
 
     // comment this chatProps if switching to react-chat-engine-pretty
-    const chatProps = useMultiChatLogic(
-        projectId,
-        user.username,
-        user.secret
-    );
+    // const chatProps = useMultiChatLogic(
+    //     projectId,
+    //     user.username,
+    //     user.secret
+    // );
 
     return (
         <div style={{ height: '100vh' }}>
-            {/* <PrettyChatWindow 
-                projectId={projectId}
-                username={user.username}
-                secret={user.secret}
-                style={{height: '100%'}}
-            /> */}
-
-            <ChatEngine
+            <PrettyChatWindow 
                 projectId={projectId}
                 username={user.username}
                 secret={user.secret}
                 style={{height: '100%'}}
             />
+
+          
 
             {/*             
             <MultiChatSocket {...chatProps} />
